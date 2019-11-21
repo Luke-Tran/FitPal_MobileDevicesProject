4b2982bff1741e93e0de5f14a82b8fd5a6fee47b
Luke-Tran <luketran08@gmail.com>
2019-11-21 16:45:01 -0500

    List of workouts updates when the user adds to it

--- lib/workouts.dart
+++ lib/workouts.dart
@@ -6,7 +6,8 @@ import 'notifications.dart';
 import 'workout_tile.dart';
 
 class Workouts extends StatefulWidget {
-  List<WorkoutTile> workoutTiles = [];
+  //List<WorkoutTile> workoutTiles = [];
+  List<Widget> workoutTiles = [];
   bool isLoaded = false;
 
   Workouts({Key key}) : super(key: key);
@@ -25,9 +26,86 @@ class _WorkoutsState extends State<Workouts> {
   final _model = DBModel();
   Color addWorkoutColor = Colors.black54;
 
+  @override
+  Widget build(BuildContext context) {
+    if (!widget.isLoaded) listWorkouts();
+
+    _notifications.init();
+    // double addBtnPadding = 14.0;
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(10.0),
+        child: ListView.builder(
+          itemCount: widget.workoutTiles.length,
+          itemBuilder: (context, i) {
+            return widget.workoutTiles[i];
+          },
+          scrollDirection: Axis.vertical,
+          shrinkWrap: true,
+        ),
+        // child: ListView(
+        //   children: <Widget> [
+        //     // GestureDetector(
+        //     //   onTapDown: (tap) {
+        //     //     setState(() {
+        //     //       addWorkoutColor = Colors.black87;
+        //     //     });
+        //     //   },
+        //     //   onTapUp: (tap) {
+        //     //     _addWorkout(context);
+        //     //     setState(() {
+        //     //       addWorkoutColor = Colors.black54;
+        //     //     });
+        //     //   },
+        //     //   onTapCancel: () {
+        //     //     setState(() {
+        //     //       addWorkoutColor = Colors.black54;
+        //     //     });
+        //     //   },
+        //     //   child: Container(
+        //     //     padding: EdgeInsets.all(addBtnPadding),
+        //     //     decoration: BoxDecoration(
+        //     //       color:  addWorkoutColor,
+        //     //     ),
+        //     //     child: Row(
+        //     //       children: <Widget>[
+        //     //         Icon(
+        //     //           Icons.add,
+        //     //           color: Colors.white,
+        //     //         ),
+        //     //         SizedBox(width: addBtnPadding),
+        //     //         Text(
+        //     //           'Add a workout...',
+        //     //           style: TextStyle(
+        //     //             color: Colors.white,
+        //     //             fontSize: 18.0,
+        //     //           ),
+        //     //         ),
+        //     //       ],
+        //     //     ),
+        //     //   ),
+        //     // ),
+        //     // SizedBox(height: 10.0),
+        //     //exercise1.build(),
+        //     //exercise2.build(),
+        //     // ListView.builder(
+        //     //   itemCount: widget.workoutTiles.length,
+        //     //   itemBuilder: (context, i) {
+        //     //     return widget.workoutTiles[i];
+        //     //   },
+        //     //   scrollDirection: Axis.vertical,
+        //     //   shrinkWrap: true,
+        //     // ),
+        //   ],
+        // ),
+      ),
+    );
+  }
+
   Future<void> listWorkouts() async {
     List<Workout> workouts = await _model.getAllWorkouts();
-    List<WorkoutTile> workoutTiles = [];
+    //List<WorkoutTile> workoutTiles = [];
+    List<Widget> workoutTiles = [addWorkoutButton(), SizedBox(height: 10.0),];
     for (Workout workout in workouts) {
       workoutTiles.add(WorkoutTile(workout: workout,));
     }
@@ -36,81 +114,19 @@ class _WorkoutsState extends State<Workouts> {
     setState(() {});
   }
 
-  @override
-  Widget build(BuildContext context) {
-    if (!widget.isLoaded) listWorkouts();
-
-    _notifications.init();
-    double addBtnPadding = 14.0;
-    return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(10.0),
-        child: Column(
-          children: <Widget> [
-            GestureDetector(
-              onTapDown: (tap) {
-                setState(() {
-                  addWorkoutColor = Colors.black87;
-                });
-              },
-              onTapUp: (tap) {
-                _addWorkout(context);
-                setState(() {
-                  addWorkoutColor = Colors.black54;
-                });
-              },
-              onTapCancel: () {
-                setState(() {
-                  addWorkoutColor = Colors.black54;
-                });
-              },
-              child: Container(
-                padding: EdgeInsets.all(addBtnPadding),
-                decoration: BoxDecoration(
-                  color:  addWorkoutColor,
-                ),
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.add,
-                      color: Colors.white,
-                    ),
-                    SizedBox(width: addBtnPadding),
-                    Text(
-                      'Add a workout...',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18.0,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            SizedBox(height: 10.0),
-            //exercise1.build(),
-            //exercise2.build(),
-            ListView.builder(
-              itemCount: widget.workoutTiles.length,
-              itemBuilder: (context, i) {
-                return widget.workoutTiles[i];
-              },
-              scrollDirection: Axis.vertical,
-              shrinkWrap: true,
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
   Future<void> _addWorkout(BuildContext context) async {
     var event = await Navigator.pushNamed(context, '/workoutform');
     if (event != null) {
-      Workout newWorkout = event;
+      List data = event;
+      Workout newWorkout = data[0];
       print(newWorkout.toString());
-      _notificationLater(event);
-      _lastInsertedId = await _model.insertWorkout(event);
+      _notificationLater(newWorkout);
+      if (data[1] == 'add') {
+        _lastInsertedId = await _model.insertWorkout(newWorkout);
+      }
+      setState(() {
+        widget.isLoaded = false;
+      });
     }
   }
 
@@ -122,4 +138,47 @@ class _WorkoutsState extends State<Workouts> {
     //await _notifications.sendNotificationLater('Workout Reminder', 'dont forget to complete your workout:'+workout.workout, workout.datetime, 'payload');
   }
 
+  GestureDetector addWorkoutButton() {
+    double addBtnPadding = 14.0;
+    return GestureDetector(
+      onTapDown: (tap) {
+        setState(() {
+          addWorkoutColor = Colors.black87;
+        });
+      },
+      onTapUp: (tap) {
+        _addWorkout(context);
+        setState(() {
+          addWorkoutColor = Colors.black54;
+        });
+      },
+      onTapCancel: () {
+        setState(() {
+          addWorkoutColor = Colors.black54;
+        });
+      },
+      child: Container(
+        padding: EdgeInsets.all(addBtnPadding),
+        decoration: BoxDecoration(
+          color:  addWorkoutColor,
+        ),
+        child: Row(
+          children: <Widget>[
+            Icon(
+              Icons.add,
+              color: Colors.white,
+            ),
+            SizedBox(width: addBtnPadding),
+            Text(
+              'Add a workout...',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18.0,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
