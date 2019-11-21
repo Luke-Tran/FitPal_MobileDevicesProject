b6b8476d436cc9fb65cf865a3d104d07173a3d24
Luke-Tran <luketran08@gmail.com>
2019-11-21 13:44:56 -0500

    Added ability for user to set the workout duration

--- lib/workout_form.dart
+++ lib/workout_form.dart
@@ -20,6 +20,13 @@ class _WorkoutFormState extends State<WorkoutForm> {
   String numSets = '''
   [[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]]
   ''';
+  String numMinutes = '''
+  [
+    [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,131,132,133,134,135,136,137,138,139,140,141,142,143,144,145,146,147,148,149,150,151,152,153,154,155,156,157,158,159,160,161,162,163,164,165,166,167,168,169,170,171,172,173,174,175,176,177,178,179,180],
+    ["min"]
+  ]
+  ''';
+
   Color repsButtonColor = Colors.white;
   Color setsButtonColor = Colors.white;
   Color durationButtonColor = Colors.white;
@@ -265,6 +272,7 @@ class _WorkoutFormState extends State<WorkoutForm> {
       break;
       case 'Duration': {
         //todo add duration dropdown list
+        _durationPicker();
       }
       break;
       case 'Due date': {
@@ -298,6 +306,18 @@ class _WorkoutFormState extends State<WorkoutForm> {
     ).showDialog(context);
   }
 
+  void _durationPicker() {
+    Picker(
+      adapter: PickerDataAdapter<String>(pickerdata: new JsonDecoder().convert(numMinutes), isArray: true),
+      hideHeader: true,
+      onConfirm: (Picker picker, List value) {
+        setState(() {
+          _duration = int.parse(picker.getSelectedValues()[0]);
+        });
+      }
+    ).showDialog(context);
+  }
+
   //workout due date picker
   Future<Null> _selectDate(BuildContext context) async {
     final DateTime picked = await showDatePicker(
@@ -326,7 +346,13 @@ class _WorkoutFormState extends State<WorkoutForm> {
       }
       break;
       case 'Duration': {
-        text = '$_duration';
+        if (_duration <= 60) {
+          text = '$_duration min';
+        } else {
+          int hours = (_duration/60.0).floor();
+          int minutes = (_duration % 60.0).floor();
+          text = '${hours}h $minutes min';
+        }
       }
       break;
       case 'Due date': {
