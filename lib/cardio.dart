b44e88c1d18dc3fa2e4f9e32e0ffa0d7bdf5d919
Gajan Sivanesan <gajan.sivanesan@ontariotechu.net>
2019-11-23 16:43:40 -0500

    Added map, button to start a timer and a marker to show user position

--- lib/cardio.dart
+++ lib/cardio.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong/latlong.dart';
 
 class Cardio extends StatefulWidget {
 
@@ -9,9 +11,61 @@ class Cardio extends StatefulWidget {
 }
 
 class _CardioState extends State<Cardio> {
+
+  final centre = LatLng(43.9457842,-78.895896); // TODO: Change to user's current location
+
+
   Widget build(BuildContext context) {
-    return Scaffold (
-      body: Text('Cardio'),
+    return Scaffold(
+      body: Stack(
+        children: <Widget> [
+          FlutterMap(
+            options: MapOptions(
+              center: centre, 
+              minZoom: 50.0,
+            ),
+            layers: [
+            TileLayerOptions(
+              // TODO: grant permissions to download the image map
+         // urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+         // subdomains: ['a', 'b', 'c'], 
+              urlTemplate:'https://api.mapbox.com/styles/v1/schere/cjzcobx1x2csf1cmppuyzj5ys/tiles/256/{z}/{x}/{y}@2x?access_token=sk.eyJ1Ijoic2NoZXJlIiwiYSI6ImNrM2MxdTBwaDBobzczZnBiaXI4ZTNmZGQifQ.jtLis4IAkXOk_Hqv81P8rw',
+              additionalOptions: {
+              'accessToken': 'sk.eyJ1Ijoic2NoZXJlIiwiYSI6ImNrM2MxdTBwaDBobzczZnBiaXI4ZTNmZGQifQ.jtLis4IAkXOk_Hqv81P8rw',
+              'id': 'mapbox.mapbox-streets-v8'
+            }
+        ),
+
+        MarkerLayerOptions(
+          markers: [
+            Marker(
+              width: 45.0,
+              height: 45.0,
+              point: centre,
+              builder: (context) => Container(
+                child: IconButton(
+                  icon: Icon(Icons.location_on),
+                  color: Colors.blue,
+                  iconSize: 45.0,
+                  onPressed: () {
+                    print('Icon Pressed');
+                  },
+                  ),
+              )
+            )
+        ])
+      ],
+       
+      ),
+        RaisedButton(
+          child: Text('Start Running'),
+          onPressed: () {
+            print('Start Button Pressed');
+          } ,)
+        ]
+      ),
+      
+        
      );
   }
 }
