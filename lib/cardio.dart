4b8e29803a3c4e13e249687c12e6c2efae4eaee0
Gajan Sivanesan <gajan.sivanesan@ontariotechu.net>
2019-12-01 07:08:45 -0500

    added map and marker to the map

--- lib/cardio.dart
+++ lib/cardio.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong/latlong.dart';
+import 'map.dart';
+
 
 class Cardio extends StatefulWidget {
 
@@ -12,60 +12,11 @@ class Cardio extends StatefulWidget {
 
 class _CardioState extends State<Cardio> {
 
-  final centre = LatLng(43.9457842,-78.895896); // TODO: Change to user's current location
 
-
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: <Widget> [
-          FlutterMap(
-            options: MapOptions(
-              center: centre, 
-              minZoom: 50.0,
-            ),
-            layers: [
-            TileLayerOptions(
-              // TODO: grant permissions to download the image map
-         // urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-         // subdomains: ['a', 'b', 'c'], 
-              urlTemplate:'https://api.mapbox.com/styles/v1/schere/cjzcobx1x2csf1cmppuyzj5ys/tiles/256/{z}/{x}/{y}@2x?access_token=sk.eyJ1Ijoic2NoZXJlIiwiYSI6ImNrM2MxdTBwaDBobzczZnBiaXI4ZTNmZGQifQ.jtLis4IAkXOk_Hqv81P8rw',
-              additionalOptions: {
-              'accessToken': 'sk.eyJ1Ijoic2NoZXJlIiwiYSI6ImNrM2MxdTBwaDBobzczZnBiaXI4ZTNmZGQifQ.jtLis4IAkXOk_Hqv81P8rw',
-              'id': 'mapbox.mapbox-streets-v8'
-            }
-        ),
-
-        MarkerLayerOptions(
-          markers: [
-            Marker(
-              width: 45.0,
-              height: 45.0,
-              point: centre,
-              builder: (context) => Container(
-                child: IconButton(
-                  icon: Icon(Icons.location_on),
-                  color: Colors.blue,
-                  iconSize: 45.0,
-                  onPressed: () {
-                    print('Icon Pressed');
-                  },
-                  ),
-              )
-            )
-        ])
-      ],
-       
-      ),
-        RaisedButton(
-          child: Text('Start Running'),
-          onPressed: () {
-            print('Start Button Pressed');
-          } ,)
-        ]
-      ),
-      
-        
-     );
+    return Scaffold (
+        body: Map(),
+    );
   }
 }
---
+++ lib/map.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:latlong/latlong.dart';
+import 'package:flutter_map/flutter_map.dart';
+// import 'location.dart';
+
+class Map extends StatelessWidget {
+  final centre = LatLng(43.9457842,-78.895896);
+  final path = [LatLng(43.9457842,-78.893896), LatLng(43.9437842,-78.897896), LatLng(43.9457842,-78.895896)];
+
+ // var location1 = new Location.Local('UOIT', 'Simcoe', (43.9457842,-78.893896));
+  //var location2 = new Location.Local('UOIT', 'Simcoe', (43.9437842,-78.897896));
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FlutterMap(
+        options:MapOptions(
+          center:centre,
+          minZoom: 16.0,
+          maxZoom: 50.0, 
+        ),
+        layers: [
+          TileLayerOptions(
+          urlTemplate: "https://api.tiles.mapbox.com/v4/"
+              "{id}/{z}/{x}/{y}@2x.png?access_token={accessToken}",
+          additionalOptions: {
+            'accessToken': 'pk.eyJ1Ijoic2NoZXJlIiwiYSI6ImNrM2Z5Y2tmdzAwNDgzbmxvNHNibjR5YmoifQ.wzTwzLrrp19Yuqh96yo6gw',
+            'id': 'mapbox.streets',
+          },
+        ),
+         MarkerLayerOptions(
+           markers:[
+             Marker(
+               width: 45.0,
+               height: 45.0,
+               point: centre,
+               builder: (context) => Container(
+                 child: IconButton(
+                   icon: Icon(Icons.location_on),
+                   color: Colors.grey,
+                   iconSize: 45.0,
+                 ),
+               )
+             )
+           ]) 
+        ]
+      )
+      );
+  }
+
+}
