adf54885571b1765428d31c75bed445df2b97adc
Luke-Tran <luketran08@gmail.com>
2019-12-08 15:52:00 -0500

    Made sign in screen look better

--- lib/pages/sign_in_page.dart
+++ lib/pages/sign_in_page.dart
@@ -10,50 +10,65 @@ class SignInPage extends StatefulWidget {
 
 class _SignInPageState extends State<SignInPage> {
   final _model = DBModel();
-  String _buttonText = 'Sign-in with Google';
+  String _buttonText = 'Sign in with Google';
   String _pageTitle = 'Sign in';
   GoogleSignIn _googleSignIn = GoogleSignIn(scopes: ['email']);
 
   _login() async {
     try {
       await _googleSignIn.signIn();
-      print(_googleSignIn.currentUser.displayName);
-      print(_googleSignIn.currentUser.email);
-      globals.isLoggedIn = true;
       globals.userEmail = _googleSignIn.currentUser.email;
       globals.profilePic = _googleSignIn.currentUser.photoUrl;
+      globals.isLoggedIn = true;
       await _model.getWorkoutsFromCloud();
       setState(() {
-        _buttonText = 'Signed in';
-        _pageTitle = 'Signed in as'
+        _buttonText = 'Sign out';
+        _pageTitle = 'Signed in as';
       });
     } catch(error) {
       print(error);
+      setState(() {
+        _pageTitle = 'Sign in';                        
+      });
+
     }
   }
 
   _logout() {
     _googleSignIn.signOut();
+    globals.userEmail = '';
+    globals.profilePic = '';
+    globals.isLoggedIn = false;
     setState(() {
       _buttonText = 'Sign in with Google';
+      _pageTitle = 'Sign in';
     });
-    globals.isLoggedIn = false;
+  }
+
+  Widget _getProfilePic(String url) {
+    if (url != "") {
+      return CircleAvatar(
+        radius: 45.0,
+        backgroundImage: NetworkImage(url),
+      );
+    }
+    else {
+      return SizedBox(height: 0.0);
+    }
+  }
+
+  List<Widget> _getButtonChildren(String buttonText) {
+    List<Widget> widgetList = [];
+    if (!globals.isLoggedIn) {
+      widgetList.add(Image(image: AssetImage("assets/google_logo.png"), height: 20.0));
+      widgetList.add(SizedBox(width: 10.0));
+    }
+    widgetList.add(Text(buttonText));
+    return widgetList;
   }
 
   @override
   Widget build(BuildContext context) {
-    Widget _getProfilePic(String url) {
-      if (url != "") {
-        return CircleAvatar(
-          radius: 45.0,
-          backgroundImage: NetworkImage(url),
-        );
-      }
-      else {
-        return SizedBox(height: 0.0);
-      }
-    }
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Sign-in'),
@@ -66,27 +81,36 @@ class _SignInPageState extends State<SignInPage> {
             children: <Widget>[
               Text(
                 _pageTitle,
+                style: TextStyle(fontSize: 24.0),
               ),
+              Text(globals.userEmail),
+              SizedBox(height: 20.0),
               _getProfilePic(globals.profilePic),
               SizedBox(height: 10.0),
-              OutlineButton(
-                splashColor: Colors.grey,
-                onPressed: () {
-                  if (!globals.isLoggedIn) {
-                    print('logging in');
-                    _login();
-                  }
-                },
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
-                borderSide: BorderSide(color: Colors.grey),
-                child: Padding(
-                  padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      Text(_buttonText),
-                    ],
+              ButtonTheme(
+                minWidth: 130.0,
+                child: OutlineButton(
+                  splashColor: Colors.grey,
+                  onPressed: () {
+                    if (!globals.isLoggedIn) {
+                      print('logging in');
+                      setState(() {
+                        _pageTitle = 'Signing in...';                        
+                      });
+                      _login();
+                    } else {
+                      _logout();
+                    }
+                  },
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+                  borderSide: BorderSide(color: Colors.grey),
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.min,
+                      children: _getButtonChildren(_buttonText),
+                    ),
                   ),
                 ),
               ),
